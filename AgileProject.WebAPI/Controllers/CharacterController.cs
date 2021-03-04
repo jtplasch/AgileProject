781ec778f430c8c79ba7e9f421c41e2a97731e07
Your Name <you@example.com>
2021-03-04 10:53:26 -0600

    charController

---
+++ AgileProject.WebAPI/Controllers/CharacterController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace AgileProject.WebAPI.Controllers
+{
+    [Authorize]
+    public class CharacterController : ApiController
+    {
+        private CharacterService CreateCharacterService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var characterService = new CharacterService(userId);
+            return characterService;
+        }
+
+        public IHttpActionResult Get()
+        {
+            CharacterService characterService = CreateCharacterService();
+            var character = characterService.GetCharacters();
+            return Ok(character);
+        }
+
+        public IHttpActionResult Post(CharacterCreate character)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateCharacterService();
+
+            if (!service.CreateCharacter(character))
+                return InternalServerError();
+
+            return Ok();
+        }
+    }
+}
