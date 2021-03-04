f53e451fd43c34be68c3cecb1e4e65cda69f72bc
Your Name <you@example.com>
2021-03-04 10:22:11 -0600

    initial

---
+++ AgileProject.Models/CharacterCreate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgileProject.Models
+{
+    public class CharacterCreate
+    {
+        [Required]
+        [MinLength(2, ErrorMessage = "Thou must have a name...")]
+        [MaxLength(16, ErrorMessage = "Whoa, settle down there bud.")]
+        public string Name { get; set; }
+        [Required]
+        public TypeOfClass Class { get; set; }
+    }
+}
